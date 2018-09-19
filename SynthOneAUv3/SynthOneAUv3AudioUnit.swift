d0caab2f8ede353a5620479ba66524051e675815
Aurelius Prochazka <aurephd@gmail.com>
2018-09-19 16:54:38 -0700

    Gene De LIsa's stuff

--- SynthOneAUv3/SynthOneAUv3AudioUnit.swift
+++ SynthOneAUv3/SynthOneAUv3AudioUnit.swift
@@ -9,6 +9,7 @@
 import AVFoundation
 import AudioKit
 import CoreAudioKit
+import AudioToolbox
 
 class SynthOneAUv3AudioUnit: AUAudioUnit {
 
@@ -16,9 +17,19 @@ class SynthOneAUv3AudioUnit: AUAudioUnit {
     private var _internalRenderBlock: AUInternalRenderBlock!
     private var conductor: Conductor!
 
+    var currentTempo = 0.0
+    var transportStateIsMoving = false
+    var mcb: AUHostMusicalContextBlock?
+    var tsb: AUHostTransportStateBlock?
+    var moeb: AUMIDIOutputEventBlock?
+
+
     override init(componentDescription: AudioComponentDescription, options: AudioComponentInstantiationOptions) throws {
 
-        self._internalRenderBlock = { (actionFlags, timeStamp, frameCount, outputBusNumber, outputData, renderEvent, pullInputBlock) in
+        try super.init(componentDescription: componentDescription, options: options)
+
+        self._internalRenderBlock = {[unowned self] (actionFlags, timeStamp, frameCount, outputBusNumber, outputData, renderEvent, pullInputBlock) in
+
             if renderEvent != nil {
 
                 let data = renderEvent!.pointee.MIDI.data
@@ -42,6 +53,71 @@ class SynthOneAUv3AudioUnit: AUAudioUnit {
 
                 }
             }
+
+            if let mcb = self.mcb {
+                var timeSignatureNumerator = 0.0
+                var timeSignatureDenominator = 0
+                var currentBeatPosition = 0.0
+                var sampleOffsetToNextBeat = 0
+                var currentMeasureDownbeatPosition = 0.0
+
+                if mcb( &self.currentTempo, &timeSignatureNumerator, &timeSignatureDenominator, &currentBeatPosition, &sampleOffsetToNextBeat, &currentMeasureDownbeatPosition ) {
+                    if let s = Conductor.sharedInstance.synth {
+                        s.setSynthParameter(.arpRate, self.currentTempo)
+                    }
+//                    NSLog("current tempo %f", self.currentTempo)
+//                    NSLog("timeSignatureNumerator %f", timeSignatureNumerator)
+//                    NSLog("timeSignatureDenominator %ld", timeSignatureDenominator)
+
+                    if self.transportStateIsMoving {
+//                        NSLog("currentBeatPosition %f", currentBeatPosition);
+//                        NSLog("sampleOffsetToNextBeat %ld", sampleOffsetToNextBeat);
+//                        NSLog("currentMeasureDownbeatPosition %f", currentMeasureDownbeatPosition);
+                    }
+                }
+
+            }
+
+            if let tsb = self.tsb {
+                var flags: AUHostTransportStateFlags = []
+                var currentSamplePosition = 0.0
+                var cycleStartBeatPosition = 0.0
+                var cycleEndBeatPosition = 0.0
+
+                if tsb(&flags, &currentSamplePosition, &cycleStartBeatPosition, &cycleEndBeatPosition) {
+
+                    if flags.contains(AUHostTransportStateFlags.changed) {
+//                        NSLog("AUHostTransportStateChanged bit set")
+//                        NSLog("currentSamplePosition %f", currentSamplePosition)
+                    }
+
+                    if flags.contains(AUHostTransportStateFlags.moving) {
+//                        NSLog("AUHostTransportStateMoving bit set");
+//                        NSLog("currentSamplePosition %f", currentSamplePosition)
+
+                        self.transportStateIsMoving = true
+
+                    } else {
+                        self.transportStateIsMoving = false
+                    }
+
+                    if flags.contains(AUHostTransportStateFlags.recording) {
+//                        NSLog("AUHostTransportStateRecording bit set")
+//                        NSLog("currentSamplePosition %f", currentSamplePosition)
+                    }
+
+                    if flags.contains(AUHostTransportStateFlags.cycling) {
+//                        NSLog("AUHostTransportStateCycling bit set")
+//                        NSLog("currentSamplePosition %f", currentSamplePosition)
+//                        NSLog("cycleStartBeatPosition %f", cycleStartBeatPosition)
+//                        NSLog("cycleEndBeatPosition %f", cycleEndBeatPosition)
+                    }
+
+                }
+            }
+
+
+
             _ = AudioKit.engine.manualRenderingBlock(frameCount, outputData, nil)
 
             return noErr
@@ -50,9 +126,6 @@ class SynthOneAUv3AudioUnit: AUAudioUnit {
         do {
             try AudioKit.engine.enableManualRenderingMode(.realtime, format: AudioKit.format, maximumFrameCount: 4_096)
             conductor = Conductor.sharedInstance
-//            conductor.start()
-
-            try super.init(componentDescription: componentDescription, options: options)
             let bus = try AUAudioUnitBus(format: AudioKit.format)
             self._outputBusArray = AUAudioUnitBusArray(audioUnit: self, busType: AUAudioUnitBusType.output, busses: [bus])
         } catch {
@@ -72,6 +145,29 @@ class SynthOneAUv3AudioUnit: AUAudioUnit {
     override var outputBusses: AUAudioUnitBusArray {
         return self._outputBusArray
     }
+
+
+    override func allocateRenderResources() throws {
+        do {
+            try super.allocateRenderResources()
+        } catch {
+            return
+        }
+
+        self.mcb = self.musicalContextBlock
+        self.tsb = self.transportStateBlock
+        self.moeb = self.midiOutputEventBlock
+
+    }
+
+    override func deallocateRenderResources() {
+        super.deallocateRenderResources()
+        self.mcb = nil
+        self.tsb = nil
+        self.moeb = nil
+    }
+
+
 
     override var internalRenderBlock: AUInternalRenderBlock {
         return self._internalRenderBlock
